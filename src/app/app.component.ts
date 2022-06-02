ae347dd60f7d201bae5172423c2cb37ac1700579
vMau12 <victormaury.pro@outlook.fr>
2022-06-02 13:12:35 +0200

    Ajout doc maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,14 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'poi-front';
+  // Options de la carte
+  // On met la position du POI en attribut center de façon à être centré dessus
   mapOptions: google.maps.MapOptions = {
     center: { lat: 38.9987208, lng: -77.2538699 },
     zoom : 14
  }
- marker = {
+  // Attributs du marker, le plus important est la position
+  marker = {
     position: { lat: 38.9987208, lng: -77.2538699 },
  }
 }
